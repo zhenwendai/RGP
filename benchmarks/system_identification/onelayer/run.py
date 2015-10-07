c6a7bfbcdb80812a4f9f634b4dd025f720399cf5
Zhenwen Dai <z.dai@sheffield.ac.uk>
2015-10-07 13:51:25 +0100

    change the default onelayer experiment

--- benchmarks/system_identification/onelayer/run.py
+++ benchmarks/system_identification/onelayer/run.py
@@ -1,6 +1,6 @@
 import autoreg.benchmark
 from autoreg.benchmark.run import run
-from autoreg.benchmark.methods import Autoreg_onelayer
+from autoreg.benchmark.methods import Autoreg_onelayer_bfgs
 from autoreg.benchmark.tasks import all_tasks
 from autoreg.benchmark.evaluation import RMSE
 from autoreg.benchmark.outputs import PickleOutput, CSV_Summary
@@ -9,7 +9,7 @@ outpath = '.'
 prjname = 'autoreg'
 config = {
           'evaluations':[RMSE,'time'],
-          'methods':[Autoreg_onelayer],
+          'methods':[Autoreg_onelayer_bfgs],
           'tasks': all_tasks,
           'repeats': 5,
           'outputs': [PickleOutput(outpath, prjname), CSV_Summary(outpath, prjname)]
